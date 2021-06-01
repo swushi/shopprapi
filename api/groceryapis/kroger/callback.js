199972f7ae78ea6f590aeaa5ae39436c6020cd43
Sam Autrey <samautrey7@gmail.com>
2021-06-01 17:22:11 -0500

    rename

--- api/groceryapis/kroger/callback.js
+++ api/groceryapis/kroger/callback.js
@@ -4,9 +4,9 @@ const db = require('../../db');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const token = req.query.code;
-  if (token) {
-    return res.status(200).json({ message: 'Successfully Authorized to use Kroger', kroger_token: token })
+  const code = req.query.code;
+  if (code) {
+    return res.status(200).json({ message: 'Successfully Authorized to use Kroger', code: code })
   }
   return res.status(400).json({ message: 'Failed to Authorize with Kroger' })
 })
